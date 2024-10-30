28e4c48d18cf8883ab3ff77d9a8ef3a2777a29ed
pasted0 <pastedv1ontop@gmail.com>
2024-10-30 19:49:17 +0000

    do english ❌ do this shit ✅

--- CustomModules/6872265039.lua
+++ CustomModules/6872265039.lua
@@ -78,7 +78,7 @@ local function GetURL(scripturl)
 	if shared.VapeDeveloper then
 		return readfile("vape/"..scripturl)
 	else
-		return game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/"..scripturl, true)
+		return game:HttpGet("https://raw.githubusercontent.com/pasted0/PastedForVapeV4Eoblpx"..scripturl, true)
 	end
 end
 
