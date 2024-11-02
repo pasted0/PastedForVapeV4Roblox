645371479f6890975626c794c9b8aafcdbe6ca3c
pasted0 <pastedv1ontop@gmail.com>
2024-11-02 05:11:31 +0000

    w

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -56,7 +56,7 @@ end
 
 if not shared.VapeDeveloper then 
 	local commit = "main"
-	for i,v in pairs(game:HttpGet("https://github.com/7GrandDadPGN/VapeV4ForRoblox"):split("\n")) do 
+	for i,v in pairs(game:HttpGet("https://raw.githubusercontent.com/pasted0/PastedForVapeV4Roblox/refs/heads/main"):split("\n")) do 
 		if v:find("commit") and v:find("fragment") then 
 			local str = v:split("/")[5]
 			commit = str:sub(0, str:find('"') - 1)
