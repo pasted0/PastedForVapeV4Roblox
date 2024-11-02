039e57423b0d880752da420eb14af61d9e10b4ce
pasted0 <pastedv1ontop@gmail.com>
2024-11-02 01:51:27 +0000

    added an antihit

--- CustomModules/6872274481.lua
+++ CustomModules/6872274481.lua
@@ -9774,67 +9774,6 @@ run(function()
 	}) 
 end)
 
-local GodMode = {Enabled = false}
-	GodMode = GuiLibrary.ObjectsThatCanBeSaved.BlatantWindow.Api.CreateOptionsButton({
-		Name = "AntiHit/Godmode",
-		Function = function(callback)
-			if callback then
-				spawn(function()
-					while task.wait() do
-						if (not GodMode.Enabled) then return end
-						if (not GuiLibrary.ObjectsThatCanBeSaved.FlyOptionsButton.Api.Enabled) and (not GuiLibrary.ObjectsThatCanBeSaved.InfiniteFlyOptionsButton.Api.Enabled) then
-							for i, v in pairs(game:GetService("Players"):GetChildren()) do
-								if v.Team ~= lplr.Team and IsAlive(v) and IsAlive(lplr) then
-									if v and v ~= lplr then
-										local TargetDistance = lplr:DistanceFromCharacter(v.Character:FindFirstChild("HumanoidRootPart").CFrame.p)
-										if TargetDistance < 25 then
-											if not lplr.Character.HumanoidRootPart:FindFirstChildOfClass("BodyVelocity") then
-												repeat task.wait() until shared.GlobalStore.matchState ~= 0
-												if not (v.Character.HumanoidRootPart.Velocity.Y < -10*5) then
-													lplr.Character.Archivable = true
-			
-													local Clone = lplr.Character:Clone()
-													Clone.Parent = workspace
-													Clone.Head:ClearAllChildren()
-													gameCamera.CameraSubject = Clone:FindFirstChild("Humanoid")
-				
-													for i,v in pairs(Clone:GetChildren()) do
-														if string.lower(v.ClassName):find("part") and v.Name ~= "HumanoidRootPart" then
-															v.Transparency = 1
-														end
-														if v:IsA("Accessory") then
-															v:FindFirstChild("Handle").Transparency = 1
-														end
-													end
-				
-													lplr.Character.HumanoidRootPart.CFrame = lplr.Character.HumanoidRootPart.CFrame + Vector3.new(0,100000,0)
-				
-													game:GetService("RunService").RenderStepped:Connect(function()
-														if Clone ~= nil and Clone:FindFirstChild("HumanoidRootPart") then
-															Clone.HumanoidRootPart.Position = Vector3.new(lplr.Character.HumanoidRootPart.Position.X, Clone.HumanoidRootPart.Position.Y, lplr.Character.HumanoidRootPart.Position.Z)
-														end
-													end)
-				
-													task.wait(0.3)
-													lplr.Character.HumanoidRootPart.Velocity = Vector3.new(lplr.Character.HumanoidRootPart.Velocity.X, -1, lplr.Character.HumanoidRootPart.Velocity.Z)
-													lplr.Character.HumanoidRootPart.CFrame = Clone.HumanoidRootPart.CFrame
-													gameCamera.CameraSubject = lplr.Character:FindFirstChild("Humanoid")
-													Clone:Destroy()
-													task.wait(0.15)
-												end
-											end
-										end
-									end
-								end
-							end
-						end
-					end
-				end)
-			end
-		end
-	})
-end)
-
 run(function()
 	local MelodyExploit = {Enabled = false}
 	MelodyExploit = GuiLibrary.ObjectsThatCanBeSaved.BlatantWindow.Api.CreateOptionsButton({
@@ -10065,3 +10004,32 @@ local Disablerv2 = GuiLibrary.ObjectsThatCanBeSaved.BlatantWindow.Api({
     Default = false, 
     ExtraText = function() return " FakeLag (we ❤️ the bw anticheat)" end 
 })
+
+
+
+
+part = Instance.new("Part", game.Players.LocalPlayer.Character)
+local Antihit = GuiLibrary.ObjectsThatCanBeSaved.BlatantWindow.Api({
+    Name = "AntiHit", -- name of object
+    Function = function(callback) 
+        if callback then
+			part.Size = Vector3.new(1,1,1)
+			part.Anchored = true
+			part.CanCollide = false
+			while task.wait(0.2) do
+				part.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame + Vector3.new(0,10,0)
+				game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = prt.CFrame 
+				task.wait(0.1)
+				game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = part - Vector3.new(0,10,0)
+			else
+            game.Players.LocalPlayer.Character.Part:Destroy()
+			end
+        end
+    end,
+    HoverText = "Simple AntiHit Made By Me Pasted0",
+    Default = false,
+    ExtraText = function() return " Method Part" end 
+})
+
+
+
